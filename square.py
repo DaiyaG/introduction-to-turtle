c7518bde3fd0a3df44388490a34276861f91b3ae
DaiyaG <daiyadharm@gmail.com>
2025-03-20 17:15:17 +0000

    square

---
+++ square.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.Screen().bgcolor("light blue")
+
+sc = turtle.Screen()
+sc.setup(400, 300)
+
+turtle.title("Welcome to Turtle Window")
+
+board = turtle.Turtle()
+
+for i in range(4):
+    board.forward(100)
+    board.left(90)
+    i = i + i 
+
+turtle.done()    
